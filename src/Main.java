039929f939283eff74502b169909ae52a40847c9
alex-pakin <aleksei_pakin@mail.ru>
2022-04-24 20:59:30 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 11254;
+        int mile = 20;
+        int bonus = amount / mile;
+
+        System.out.println("Ваши бонусные мили = " + bonus);
+
+    }
+}
